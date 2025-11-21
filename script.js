ea6d1b7455ea788e2c562e16bb07ce27f01e75d8
samuel <sami2game@gmail.com>
2025-11-21 22:13:08 +0100

    Subiendo mi portfolio terminado

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Typed('#typed-text', {
+    strings: [
+      'Desarrollador Web',
+      'Diseñador UI/UX',
+      'Programador Full Stack'
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|'
+  });
+});
